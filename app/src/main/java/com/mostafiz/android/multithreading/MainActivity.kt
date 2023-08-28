acca938332b89eb6ae39356c97bb5f4d21a7a0c9
Saawry <saawry.hb@gmail.com>
2023-08-29 03:14:14 +0600

    Creating Threads by Runnable and Thread

--- app/src/main/java/com/mostafiz/android/multithreading/MainActivity.kt
+++ app/src/main/java/com/mostafiz/android/multithreading/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-
+import com.mostafiz.android.multithreading.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun stopThread(view: View) {
@@ -18,16 +20,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     /*
-    *The task is long and running on main/UI thread. After running the thread, the UI gets frozen
-    *until the thread completes the execution. during that time If user try to access any UI component,
-    *ANR occurs if the UI takes more than 5 seconds to respond.  */
+    The Solution is executing long running task in a background thread. One way of creating background
+     thread is Extending Tread class. Another way is implementing Runnable Interface and passing that in a
+     new thread.
+      */
     fun startThread(view: View) {
-        for (i in 0 until 10) {
-            Log.d("Tag_Thread_Run", "startThread: $i")
-            try {
-                Thread.sleep(1000)
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
+        val exampleThread =ExampleThread(10)
+        exampleThread.start()
+
+        val exampleRunnable =ExampleRunnable(10)
+        Thread(exampleRunnable).start()
+    }
+
+
+    class ExampleRunnable(private val counter:Int):Runnable{
+        override fun run() {
+            for (i in 0 until counter) {
+                Log.d("Tag_Thread_Run", "Runnable is running: $i")
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+    }
+
+    class ExampleThread(private val counter:Int): Thread() {
+        override fun run() {
+            for (i in 0 until counter) {
+                Log.d("Tag_Thread_Run", "Thread 1 is running: $i")
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
             }
         }
     }
